0847c2844599d3a6e99a1a0f05f32f05a02bfd65
Geonwu Lee <geonwule@c6r2s8.42seoul.kr>
2023-02-09 21:23:30 +0900

    B_2292_inig

---
+++ Bronze/2292.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, com, six = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= i * 6 + 1; j++)
+        {
+            
+        }
+    }
+    printf("%d\n", six);
+}
