87e52a369f1e269f701f474e6f6b64f9e6d1b7df
Geonwu Lee <geonwule@cx1r1s1.42seoul.kr>
2023-02-14 14:54:50 +0900

    10989_easy_version

--- Bronze/test.c
+++ Bronze/test.c
@@ -2,11 +2,7 @@
 
 int main()
 {
-	int j = 0;
-	for (int i = 0; i < 100; i++)
-	{
-		if (j >= 10)
-			j = 0;
-		printf("%d ", j++);
-	}
+	int arr[10000] = { 1, };
+	for (int i = 0; i < 10000; i++)
+		printf("%d", arr[i]);
 }
