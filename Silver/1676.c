3a4522708239e0e280a507bfdeb2a588fbc309f8
Geonwu Lee <geonwlee22@gmail.com>
2023-03-06 12:58:15 +0900

    0306

--- Silver/1676.c
+++ Silver/1676.c
@@ -10,35 +10,32 @@ int main()
         printf("0\n");
         return (0);
     }
-    int zero = 0;
-    unsigned long long  sum = 1;
-    int two = 0, five = 0;
+    int five = 0;
     for (int i = 1; i <= n; i++)
     {
-        if (i % 2 == 0)
-            two++;
         if (i % 5 == 0)
             five++;
+        if (i % 25 == 0)
+            five++;
+        if (i % 125 == 0)
+            five++;
     }
-    if (two <= five)
-        printf("%d\n", two);
-    else
-        printf("%d\n", five);
+    printf("%d\n", five);
     return (0);
-    // for (unsigned long long i = 2; i <= n; i++)
-    // {
-    //     if ((sum % 10) % 2 == 0 && i % 10 == 5
-    //     || sum % 10 == 5 && (i % 10) % 2 == 0)
-    //     {
-    //         zero++;
-    //         sum = (sum * i) / 10;
-    //     }
-    //     else if (i % 10 == 0)
-    //     {
-    //         zero++;
-    //     }
-    //     else
-    //         sum = (sum * i) % 10; 
-    // }
-   //printf("%d\n", zero);
 }
+// for (unsigned long long i = 2; i <= n; i++)
+// {
+//     if ((sum % 10) % 2 == 0 && i % 10 == 5
+//     || sum % 10 == 5 && (i % 10) % 2 == 0)
+//     {
+//         zero++;
+//         sum = (sum * i) / 10;
+//     }
+//     else if (i % 10 == 0)
+//     {
+//         zero++;
+//     }
+//     else
+//         sum = (sum * i) % 10;
+// }
+// printf("%d\n", zero);
