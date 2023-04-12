e03baa3689c2eac866decdfb2a153060a12947c5
Geonwu Lee <geonwlee22@gmail.com>
2023-04-12 20:05:40 +0900

    1629_ing

--- Silver/test.c
+++ Silver/test.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <limits.h>
 
+double	power_recul(double x, int n)
+{
+	if (n == 0)
+		return (1.0);
+	double half = power_recul(x, n / 2);
+	if (n % 2 == 0)
+		return (half * half);
+	else if (n > 0)
+		return (half * half * x);
+	else
+		return (half * half / x);
+}
+
+double	power(double x, int n)
+{
+	double result = 1.0;
+	while (n)
+	{
+		if (n % 2 == 1)
+			result *= x;
+		x *= x;
+		n /= 2;
+	}
+	return (result);
+}
+
 int main()
 {
-	printf("%lld\n", LLONG_MIN);
+	double	x =  2.0;
+	int n = 10;
+	double result = power(x, n);
+	printf("%f to the power of %d is %f\n", x, n, result);
 }
