636db8371cd2dc0978804fcaac0ea0e9b34877f0
Kannu <geonwlee22@gmail.com>
2023-10-03 21:47:10 +0900

    silver_update

--- Silver/1541_잃어버린괄호.cpp
+++ Silver/1541_잃어버린괄호.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 int main()
 {
     std::string s;
     std::cin >> s;
     int ret = 0, cur = 0;
-    char cal = '+';
+    bool minus = false;
+    std::string str;
     for (int i = 0; i < s.size(); i++)
     {
-        char c = s[i];
-        if (c == '+' || c == '-')
+        char t = s[i];
+        if (t == '-' || t == '+')
         {
-            if (cal == '-')
-                ret -= cur;
-            else // cal == '+'
-                ret += cur;
-            cal = c;
-            cur = 0;
+            int temp_int = std::stoi(str);
+            if (minus)
+                cur += temp_int;
+            else
+                ret += temp_int;
+            if (t == '-')
+                minus = true;
+            str = "";
             continue;
         }
+        str += t;
     }
+    int temp_int = std::stoi(str);
+    if (minus)
+        cur += temp_int;
+    else
+        ret += temp_int;
+    ret -= cur;
+    std::cout << ret;
 }
