07f80d9c48813511ff17f1b30eed5667f6829a07
Geonwu Lee <geonwlee22@gmail.com>
2023-10-10 16:00:44 +0900

    silver:6064 ing

---
+++ Silver/6064_카잉달력.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <cctype>
+using namespace std;
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <climits>
+#include <queue>
+#include <deque>
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int ret = 0;
+
+int mm[600][600];
+bool mv[600][600] = {
+    false,
+};
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int m, n, a, b;
+        int ret = 0;
+        cin >> m >> n >> a >> b;
+        bool no = true;
+        int x = 1, y = 1;
+        if (min(a, b) == a)
+        {
+
+        }
+        else
+        [
+            
+        ]
+
+
+        if (no)
+            cout << "-1\n";
+        else
+            cout << ret << "\n";
+    }
+}
