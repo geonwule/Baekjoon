cbc4e07077288b5a2be5f6eed82046d7a0e7a589
Kannu <geonwlee22@gmail.com>
2023-10-18 18:26:20 +0900

    silver_bfs_1389

--- Silver/1389.cpp
+++ Silver/1389.cpp
@@ -19,30 +19,15 @@ bool visited[101][101];
 
 #include <queue>
 
+#include <algorithm>
+
 struct zz
 {
-    int idx, dist;
+    int dest, comp, dist;
 };
 
-std::vector<std::queue<zz>> vq;
-
-void bfs(int b, int dest, int &bacon, int dist)
+void set_rel()
 {
-
-    for (int i = 1; i <= m; i++)
-    {
-        if (!relation[b][i] || dest == i || visited[dest][i])
-            continue;
-
-        visited[dest][i] = true;
-        bacon += dist;
-    }
-}
-
-int main()
-{
-    std::cin >> n >> m;
-    for (int i = 0; i < n; i++)
     for (int i = 0; i < m; i++)
     {
         int a, b;
@@ -50,15 +35,45 @@ int main()
         relation[a][b] = 1;
         relation[b][a] = 1;
     }
+}
 
-    int ret = -1;
-    for (int dest = 1; dest <= m; dest++)
+int main()
+{
+    std::cin >> n >> m;
+    set_rel();
+
+    std::queue<zz> q;
+    for (int i = 1; i <= n; i++)
+        q.push({i, i, 1});
+
+    std::vector<int> retArr(n + 1, 0);
+
+    while (!q.empty())
     {
-        int bacon = 0;
-        bfs(dest, dest, bacon, 1);
-        if (ret == -1 || bacon < ret)
-            ret = bacon;
+        int dest = q.front().dest;
+        int comp = q.front().comp;
+        int dist = q.front().dist;
+        q.pop();
+        for (int i = 1; i <= n; i++)
+        {
+            if (relation[comp][i] && i != dest)
+            {
+                if (!visited[dest][i])
+                {
+                    visited[dest][i] = true;
+                    retArr[dest] += dist;
+                    q.push({dest, i, dist + 1});
+                }
+            }
+        }
     }
 
+    // std::sort(retArr.begin(), retArr.end());
+    int ret = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        if (retArr[ret] > retArr[i])
+            ret = i;
+    }
     std::cout << ret;
 }
