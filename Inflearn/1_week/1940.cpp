a8f7b1b5058bc9b03106a3933410110bf6cfbb84
geonwule <geonwlee22@gmail.com>
2024-06-25 15:03:34 +0900

    1940_2

--- Inflearn/1_week/1940.cpp
+++ Inflearn/1_week/1940.cpp
@@ -7,6 +7,20 @@ int cnt = 0;
 int N, M, R = 2;
 vector<int> v;
 
+int solution2()
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = i + 1; j < v.size(); j++)
+        {
+            if (v[i] + v[j] == M)
+                cnt++;
+        }
+    }
+
+    return cnt;
+}
+
 void combi(int start, int sum, vector<int> &b)
 {
     if (b.size() == R)
@@ -42,7 +56,8 @@ int main()
     for (int &i : v)
         cin >> i;
 
-    cout << solution();
+    // cout << solution();
+    cout << solution2();
 
     return 0;
 }
