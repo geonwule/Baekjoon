81a8b6d714cfa0b54097efd6ce5eb0e097f3a0f3
geonwule <geonwlee22@gmail.com>
2024-06-25 17:39:20 +0900

    3986_stack

--- Inflearn/1_week/3986.cpp
+++ Inflearn/1_week/3986.cpp
@@ -3,19 +3,63 @@
 
 using namespace std;
 
+bool is_good(const string& s)
+{
+    int cnt_a, cnt_b;
+    cnt_a = cnt_b = 0;
+    for (const char &c : s)
+    {
+        if (c == 'A')
+            cnt_a++;
+        else
+            cnt_b++;
+    }
+    if (cnt_a % 2 == 1 || cnt_b % 2 == 1)
+        return false;
+
+    char target = 0, target2 = 0;
+    for (const char &c : s)
+    {
+        if (target == 0)
+            target = c;
+        else if (c == target && target2 != 0)
+            return false;
+        else if (c == target)
+            target = 0;
+        else
+        {
+            if (target2 == 0)
+                target2 = c;
+            else
+                target2 = 0;
+        }
+    }
+    
+    return true;
+}
+
+#include <stack>
+
 int solution(vector<string> v)
 {
     int answer = 0;
-
+    for (const string &s : v)
+    {
+        // if (is_good(s))
+        //     answer++;
+        stack<char> stk;
+        for (const char& c: s)
+        {
+            if (!stk.empty() && stk.top() == c) stk.pop();
+            else
+                stk.push(c);
+        }
+        if (stk.empty())
+            answer++;
+    }
     return answer;
 }
 
-/*문제가 이해가 안됌.
-
-평석이는 단어 위로 아치형 곡선을 그어 같은 글자끼리(A는 A끼리, B는 B끼리) 쌍을 짓기로 하였다. 
-만약 선끼리 교차하지 않으면서 각 글자를 정확히 한 개의 다른 위치에 있는 같은 글자와 짝 지을수 있다면, 
-그 단어는 '좋은 단어'이다. 평석이가 '좋은 단어' 개수를 세는 것을 도와주자.
-*/
 int main()
 {
     int N;
